1919e828b280979f5eadd3e3e7c0b167f7bc0b9e
QuyDevit <ngocquydev02@gmail.com>
2024-12-01 14:37:58 +0700

    first commit

---
+++ HistoryQuizApp/Models/EF/User.cs
@@ -0,0 +1,16 @@
+﻿namespace HistoryQuizApp.Models.EF
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string Role { get; set; } = "User"; // Default is "User"
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+
+        // Navigation property
+    }
+}
